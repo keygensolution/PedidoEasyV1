52cb385b2bfae41d4369acc103ab5f4684e5dfe3
Keygen Solution <keygensolution@gmail.com>
2022-06-10 19:39:02 -0300

    Creating the css and js files for the APP module

--- source/Controllers/App/App.php
+++ source/Controllers/App/App.php
@@ -6,22 +6,22 @@ use Source\Core\Controller;
 
 class App extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct(__DIR__ . "/../../../themes/" . CONF_VIEW_APP . "/");
-    }
+  public function __construct()
+  {
+    parent::__construct(__DIR__ . "/../../../themes/" . CONF_VIEW_APP . "/");
+  }
 
-    public function index(): void
-    {
-        $head = $this->seo->render(
-            "Home - " . CONF_SITE_NAME . " - " . CONF_SITE_TITLE,
-            CONF_SITE_DESC,
-            url(),
-            theme("/assets/images/share.jpg")
-        );
+  public function index(): void
+  {
+    $head = $this->seo->render(
+      "Home - " . CONF_SITE_NAME . " - " . CONF_SITE_TITLE,
+      CONF_SITE_DESC,
+      url(),
+      theme("/assets/images/share.jpg")
+    );
 
-        echo $this->view->render("views/App/index", [
-            "head" => $head
-        ]);
-    }
+    echo $this->view->render("views/App/index", [
+      "head" => $head
+    ]);
+  }
 }
